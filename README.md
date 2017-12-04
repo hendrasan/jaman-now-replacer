55cf494e171a8ec40d5c76a74a9035328aade958
Hendra Susanto <benhanks040888@gmail.com>
2017-12-04 21:46:12 +0700

    update code with document.createTreeWalker and MutationObserver, update Readme

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-# zaman-now-replacer
+# jaman-now-replacer
 
-Ini adalah Chrome Extension untuk mengganti frase Jaman Now dan variasinya menjadi yang seharusnya.
+Ini adalah Chrome Extension untuk mengganti frase Jaman Now dan variasinya menjadi yang seharusnya, berikut ini adalah frase-frase yang akan diubah:
+
+- #kidsjamannow
+- #kidszamannow
+- kidsjamannow
+- kidszamannow
+- kids jaman now
+- kids zaman now
+- kids jaman old
+- kids zaman old
+- jaman now
+- zaman now
+- jaman old
+- zaman old
 
 Silakan buka issue/pull request kalau menemukan variase frase jaman now lainnya.
--- content_script.js
+++ content_script.js
@@ -1,69 +1,79 @@
+// Codes are based on the one here:
+// https://github.com/mark-monteiro/drumpfinator
+
 // add an interval so it also works on dynamically loaded content
 var readyStateCheckInterval = setInterval(function() {
   if (document.readyState === "complete") {
     clearInterval(readyStateCheckInterval);
-
-    setInterval(function() {
-      walk(document.body);
-    }, 1000);
-
+    
+    main();
   }
 }, 10);
 
+function main() {
+  document.title = replaceText(document.title);
 
-function walk(node) 
-{
-  // Stolen from the this repo:
-  // https://github.com/panicsteve/cloud-to-butt
-  // Which stole this function from here:
-  // http://is.gd/mwZp7E
-  
-  var child, next;
-  
-  switch ( node.nodeType )  
-  {
-    case 1:  // Element
-    case 9:  // Document
-    case 11: // Document fragment
-      child = node.firstChild;
-      while ( child ) 
-      {
-        next = child.nextSibling;
-        walk(child);
-        child = next;
+  replaceNodeText(document.body);
+
+  // Create a mutation observer to monitor the DOM for changes
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      Array.prototype.slice.call(mutation.addedNodes).forEach(replaceNodeText);
+    });
+  });
+
+  // Configure and start the observer
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: false,
+    characterData: false
+  });
+}
+
+function replaceNodeText(node) {
+  // Create a tree walker to traverse all text nodes
+  var walker = document.createTreeWalker(
+    node,
+    NodeFilter.SHOW_TEXT,
+    {
+      acceptNode: function(node) {
+        // Reject contentEditable and textarea nodes
+        return (node.parentElement && (node.parentElement.isContentEditable || node.parentElement.nodeName == "TEXTAREA")) ?
+          NodeFilter.FILTER_SKIP :
+          NodeFilter.FILTER_ACCEPT;
       }
-      break;
+    },
+    false
+  );
 
-    case 3: // Text node
-      handleText(node);
-      break;
+  var textNode;
+  while (textNode = walker.nextNode()) {
+    textNode.nodeValue = replaceText(textNode.nodeValue);
   }
 }
 
-function handleText(textNode) 
-{
-  var v = textNode.nodeValue;
-
-  v = v.replace(/\#kidsjamannow/gi, "#anakzamansekarang");
-  v = v.replace(/\#kidszamannow/gi, "#anakzamansekarang");
-  v = v.replace(/kidsjamannow/gi, "anakzamansekarang");
-  v = v.replace(/kidszamannow/gi, "anakzamansekarang");
-  v = v.replace(/Kids Jaman Now/g, "Anak-anak Zaman Sekarang");
-  v = v.replace(/Kids jaman now/g, "Anak-anak zaman sekarang");
-  v = v.replace(/kids jaman now/gi, "anak-anak zaman sekarang");
-  v = v.replace(/Kids Jaman Old/g, "Anak-anak Zaman Dulu");
-  v = v.replace(/Kids jaman old/g, "Anak-anak zaman dulu");
-  v = v.replace(/kids jaman old/gi, "anak-anak zaman dulu");
-  v = v.replace(/Kids Zaman Now/g, "Anak-anak Zaman Sekarang");
-  v = v.replace(/Kids zaman now/g, "Anak-anak zaman sekarang");
-  v = v.replace(/kids zaman now/gi, "anak-anak zaman sekarang");
-  v = v.replace(/Kids Zaman Old/g, "Anak-anak Zaman Dulu");
-  v = v.replace(/Kids zaman old/g, "Anak-anak zaman dulu");
-  v = v.replace(/kids zaman old/gi, "anak-anak zaman dulu");
-  v = v.replace(/jaman now/gi, "zaman sekarang");
-  v = v.replace(/jaman old/gi, "zaman dulu");
-  v = v.replace(/zaman now/gi, "zaman sekarang");
-  v = v.replace(/zaman old/gi, "zaman dulu");
+function replaceText(text) {
+  text = text.replace(/\#kidsjamannow/gi, "#anakzamansekarang");
+  text = text.replace(/\#kidszamannow/gi, "#anakzamansekarang");
+  text = text.replace(/kidsjamannow/gi, "anakzamansekarang");
+  text = text.replace(/kidszamannow/gi, "anakzamansekarang");
+  text = text.replace(/Kids Jaman Now/g, "Anak-anak Zaman Sekarang");
+  text = text.replace(/Kids jaman now/g, "Anak-anak zaman sekarang");
+  text = text.replace(/kids jaman now/gi, "anak-anak zaman sekarang");
+  text = text.replace(/Kids Jaman Old/g, "Anak-anak Zaman Dulu");
+  text = text.replace(/Kids jaman old/g, "Anak-anak zaman dulu");
+  text = text.replace(/kids jaman old/gi, "anak-anak zaman dulu");
+  text = text.replace(/Kids Zaman Now/g, "Anak-anak Zaman Sekarang");
+  text = text.replace(/Kids zaman now/g, "Anak-anak zaman sekarang");
+  text = text.replace(/kids zaman now/gi, "anak-anak zaman sekarang");
+  text = text.replace(/Kids Zaman Old/g, "Anak-anak Zaman Dulu");
+  text = text.replace(/Kids zaman old/g, "Anak-anak zaman dulu");
+  text = text.replace(/kids zaman old/gi, "anak-anak zaman dulu");
+  text = text.replace(/jaman now/gi, "zaman sekarang");
+  text = text.replace(/jaman old/gi, "zaman dulu");
+  text = text.replace(/zaman now/gi, "zaman sekarang");
+  text = text.replace(/zaman old/gi, "zaman dulu");
   
-  textNode.nodeValue = v;
+  return text;
 }
